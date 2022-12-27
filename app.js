919aebc2bb4a2373180ce0dcc6cf75b3e4add4b1
St1chaKooky <vova.stoan2006@gmail.com>
2022-12-27 19:02:46 +0200

    app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+const button = document.querySelector('.buttonAdds');
+
+const listEmpty = document.querySelector('.listEmpty');
+
+const by = document.querySelector('.byList');
+
+const byInfo = by.children;
+button.addEventListener('click',buttonClick);
+let counter = 0;
+
+function buttonClick(){
+    if (counter != 0){
+
+    } else {
+
+    }
+}
