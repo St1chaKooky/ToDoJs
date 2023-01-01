0aea02dab8fc574ff73ca29b4a1c77c11692773e
St1chaKooky <vova.stoan2006@gmail.com>
2023-01-01 20:18:33 +0200

    app.js

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-const button = document.querySelector('.buttonAdds');
+function main() {
+    const input = document.querySelector('.inputText');
+    const button = document.querySelector('.buttonAdds');
+    const taskList = document.querySelector('.list')
+    const taskEmpty = document.querySelector('.listEmpty')
 
-const listEmpty = document.querySelector('.listEmpty');
+    button.addEventListener('click',buttonClick);
 
-const by = document.querySelector('.byList');
+    function buttonClick(){
 
-const byInfo = by.children;
-button.addEventListener('click',buttonClick);
-let counter = 0;
-
-function buttonClick(){
-    if (counter != 0){
-
-    } else {
-
+        const taskText = input.value;
+        const taskHtml = `
+            <div class="el">
+                <p class="textInfo">${taskText}</p>
+                <button class="checkMark"><img class="checkMarkImg" src="./imgs/mark.png"></button>
+                <button class="crossMark"><img class="crossImg" src="./imgs/cross.png"></button>
+            </div>`
+            console.log(taskHtml)
+        //Добавляем задачу на страницу
+        taskList.insertAdjacentHTML('beforeend',taskHtml)
+        //Очищаем строку ввода, оставляем фокус на инпуте
+        input.value = ""
+        input.focus()
+        //Определим есть ли у нас списки задач и скажем нужно покащзать лист пуст или неет
+        if  (taskList.children.length > 1) {
+            taskEmpty.classList.add('none')
+        }
     }
 }
+
+window.onload = main;
